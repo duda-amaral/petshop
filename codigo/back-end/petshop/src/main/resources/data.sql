525587e12d4906521230bbf6f3ad374f0b1c9bfb
duda-amaral <dudaamaralmap3110@gmail.com>
2024-08-01 19:31:40 -0300

    inserindo dados na tabela petshop

---
+++ codigo/back-end/petshop/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO petshop (nome, distancia, preco_cao_pequeno_dia_util, preco_cao_grande_dia_util, preco_cao_pequeno_fim_semana, preco_cao_grande_fim_semana) VALUES
+('Meu Canino Feliz', 2.0, 20.0, 40.0, 24.0, 48.0),
+('Vai Rex', 1.7, 15.0, 50.0, 20.0, 55.0),
+('ChowChawgas', 0.8, 30.0, 45.0, 30.0, 45.0);
