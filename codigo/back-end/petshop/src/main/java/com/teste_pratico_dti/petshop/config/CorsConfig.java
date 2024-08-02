6c8889176761cd1e35fc5736fbea11aa252eddd1
duda-amaral <dudaamaralmap3110@gmail.com>
2024-08-02 01:47:12 -0300

    modificando url

--- codigo/back-end/petshop/src/main/java/com/teste_pratico_dti/petshop/config/CorsConfig.java
+++ codigo/back-end/petshop/src/main/java/com/teste_pratico_dti/petshop/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://melhor-petshop-front.onrender.com/")
+                        .allowedOrigins("http://localhost:5173", "https://melhor-petshop-front.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
